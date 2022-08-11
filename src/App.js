4049de0fac2b6eb5babc5badc5b29f81a12ed823
abdullahomar1997 <abdullahomar19971201@gmail.com>
2022-08-12 00:20:01 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import Example1 from "./components/css_grids_Layout/grid_template_areas/example_1/Example_1";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Example1 />
     </div>
   );
 }
---
+++ src/components/css_grids_Layout/grid_template_areas/example_1/Example_1.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./index.css";
+
+const Example_1 = () => {
+  return (
+    <div className="container">
+      <header></header>
+      <nav></nav>
+      <main></main>
+      <aside></aside>
+      <footer></footer>
+    </div>
+  );
+};
+
+export default Example_1;
+
+// Learn How To Make a Responsive CSS Grid Layout
